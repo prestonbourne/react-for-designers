f708ba2fbef353fed22b5af504544ed478691cee
Saintpreston <90055250+Saintpreston@users.noreply.github.com>
2022-11-09 04:16:14 +0000

    Initial commit
    Created from https://vercel.com/new

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  themeConfig: './theme.config.js',
+  unstable_staticImage: true,
 })
+
 
 module.exports = withNextra()
