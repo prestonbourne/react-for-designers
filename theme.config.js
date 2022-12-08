28e298c0e7a6ba97829716a58ec91dfe88d60b33
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2022-12-08 16:05:11 -0500

    Fix typos

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,14 @@
 export default {
   github: 'https://github.com/Saintpreston/react-for-designers',
+  docsRepositoryBase: 'https://github.com/Saintpreston/react-for-designers',
   titleSuffix: '',
-  floatTOC: true,
+  editLink:{
+    text: ''
+  },
+  banner: {
+    
+   
+  },
   logo: (
     <>
       <span style={{fontWeight:900}}>React For Designers </span>
@@ -54,12 +61,19 @@ export default {
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
-  search: true,
-  prevLinks: true,
-  nextLinks: true,
-  footer: true,
-  // footerEditLink: 'Edit this page on Github',
-  // footerEditOnGithubLink: true,
-  footerText: <>Built by Preston with Nextra ❤️</>,
-  unstable_faviconGlyph: '❤️',
+ 
+ 
+  footer: {
+    text: <p>
+    Built by <a href="https://www.linkedin.com/in/prestonbourne/" target="_blank">Preston</a>  {new Date().getFullYear() + " "}
+      with <a href="https://www.nextra.site" target="_blank"  >Next</a> ❤️</p>
+  },
+  feedback:{
+    content: <></>
+  },
+  chat: {
+    link: 'https://www.linkedin.com/in/prestonbourne/',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>,
+  },
+ 
 }
